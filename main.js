76da03801f7ec7990aa7af9d9839259f8ca4c09e
TaksheelMohanty <72607083+TaksheelMohanty@users.noreply.github.com>
2021-03-25 21:58:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function study_page(){
+    window.location = "studytable.html";
+}
+
+function bedroom(){
+    window.location = "bedroom.html";
+}
+
+function dining(){
+    window.location = "diningroom.html";
+}
+
+function live(){
+    window.location = "livetime.html";
+}
